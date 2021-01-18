da5708ed765a91eab4d620548f30d795b43f3d90
ItsDominik <dominick9753@gmail.com>
2021-01-18 00:21:24 -0500

    Avances desde movil

---
+++ Python Course/dicctionaties.py
@@ -0,0 +1,8 @@
+datos = {"nombre":"Dominik",
+         "apellido":"Mendoza",
+         "edad":17}
+print(datos["edad"])
+print(datos["nombre"])
+print(datos["apellido"])
+datos["name"]="Dominik"
+print(datos["name"])
---
+++ Python Course/square.py
@@ -0,0 +1,4 @@
+from functions import square
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+#another way to import is just ussing the name like "import functions", but when i want to use a function i need to write "functions.(name of the function)" 
