19d49028e4b4d1b962210dfc2840ae7680053d07
ItsDominik <dominick9753@gmail.com>
2021-01-17 23:33:23 -0500

    Changes on Termux

---
+++ Python Course/loops.py
@@ -0,0 +1,10 @@
+for i in [0,1,2,3,4,5]:
+    print(i)
+for i in range(6):
+    print(i)
+names=["Harry", "Hermione", "Ginny"]
+for name in names:
+    print (name)
+nombre="Dominik"
+for character in nombre:
+    print (character)
--- Python Course/set.py
+++ Python Course/set.py
@@ -1,1 +1,11 @@
 #create a empty set
+s=set()
+#Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+s.remove(2)
+print(s)
+print(f"This set has {len(s)} elements")
