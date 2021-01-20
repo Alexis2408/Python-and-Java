6ea0e65705125c16c22ddc66b4d06c6cf93a10d3
Dominik <dominick.159.753@gmail.com>
2021-01-19 20:34:48 -0500

    Avances de la pagina

--- firstDjango/helloDom/views.py
+++ firstDjango/helloDom/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "hello/index.html")
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
+def Dominik(request):
+    return HttpResponse("Hola Dominik")
+def Alexis(request):
+    return HttpResponse("Hola Alexis")
