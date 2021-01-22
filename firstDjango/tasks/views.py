a221e1111ce8d8915fc2d02cbec597c5d2abb315
ItsDominik <dominick9753@gmail.com>
2021-01-22 17:01:53 -0500

    quedan 19 minutos

--- firstDjango/tasks/views.py
+++ firstDjango/tasks/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 tasks = ["foo", "bar", "baz"]
 # Create your views here.
 def index(request):
-    return render(request, "task/index.html", {
+    return render(request, "tasks/index.html", {
         "tasks": tasks
     })
+def add(request):
+    return render(request, "tasks/add.html")
