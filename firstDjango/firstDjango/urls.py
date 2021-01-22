af5cf4f6314aef71dd1c11bcb64c055906ff6028
ItsDominik <dominick9753@gmail.com>
2021-01-21 20:33:02 -0500

    Creacion de newyear

--- firstDjango/firstDjango/urls.py
+++ firstDjango/firstDjango/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloDom/', include("helloDom.urls"))
+    path('helloDom/', include("helloDom.urls")),
+    path('newyear/', include("newyear.urls"))
 ]
--- firstDjango/helloDom/views.py
+++ firstDjango/helloDom/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, "hello/index.html")
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name":name.capitalize()
+    })
 def Dominik(request):
     return HttpResponse("Hola Dominik")
 def Alexis(request):
